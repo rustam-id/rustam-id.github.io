ad7150ead52475c96a4c25442460e1a9761bbfeb
rustam-id <53973764+rustam-id@users.noreply.github.com>
2019-11-19 22:32:45 +0300

    Add files via upload

--- src/components/MainTable.vue
+++ src/components/MainTable.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="cont">
+    <!-- возможный контент+форма поиска/филтрации -->
     <v-card-title>
-        <a href="https://github.com/rustam-id/rustam-id.github.io" target=”_blank” class="mr-2">Git Repo</a>
+      <a
+        href="https://github.com/rustam-id/rustam-id.github.io"
+        target="”_blank”"
+        class="mr-2"
+      >Git Repo</a>
       <v-spacer></v-spacer>
       <v-text-field v-model="search" label="Поиск..." single-line hide-details color="red darken-2"></v-text-field>
     </v-card-title>
 
+    <!-- создание таблицы с указанием стартовых значений и интерфейса -->
     <v-data-table
       :headers="headers"
-      :items="carriage"
+      :items="wagons"
       :items-per-page="10"
       :search="search"
       class="elevation-1"
+      :custom-sort="customSort"
       :footer-props="{pageText: '{0}-{1} из {2}', itemsPerPageText: 'Строк на странице'}"
     >
       <template v-slot:top>
@@ -19,6 +26,8 @@
           <v-toolbar-title>Test Table</v-toolbar-title>
           <v-divider class="mx-4" inset vertical></v-divider>
           <v-spacer></v-spacer>
+
+          <!-- диалоговое модальное окно -->
           <v-dialog v-model="dialog" max-width="600px">
             <v-card>
               <v-card-text>
@@ -28,31 +37,70 @@
                       <v-text-field
                         v-model="editedItem.lastOperDtFormat"
                         label="Дата/время операции"
+                        :rules="[rules.lastOperDtValidation]"
+                        color="blue-grey darken-2"
                       ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.trainNumber" label="Номер поезда"></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.trainNumber"
+                        color="blue-grey darken-2"
+                        label="Номер поезда"
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.invoiceNumber" label="Номер накладной"></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.invoiceNumber"
+                        color="blue-grey darken-2"
+                        label="Номер накладной"
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.invoiceId" label="ИД накладной"></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.invoiceId"
+                        color="blue-grey darken-2"
+                        label="ИД накладной"
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.ordNumber" label="№ п/п" disabled></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.ordNumber"
+                        color="blue-grey darken-2"
+                        label="№ п/п"
+                        disabled
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.carNumber" label="Номер вагона" disabled></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.carNumber"
+                        color="blue-grey darken-2"
+                        label="Номер вагона"
+                        disabled
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.trainIndex" label="Индекс поезда" disabled></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.trainIndex"
+                        color="blue-grey darken-2"
+                        label="Индекс поезда"
+                        disabled
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.carStatus" label="Статус" disabled></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.carStatus"
+                        color="blue-grey darken-2"
+                        label="Статус"
+                        disabled
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.stateId" label="stateId" disabled></v-text-field>
+                      <v-text-field
+                        v-model="editedItem.stateId"
+                        color="blue-grey darken-2"
+                        label="stateId"
+                        disabled
+                      ></v-text-field>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -67,6 +115,7 @@
           </v-dialog>
         </v-toolbar>
       </template>
+      <!-- обработка события клика -->
       <template v-slot:item="{item}">
         <tr @click="editItem(item)">
           <td>{{ item.ordNumber }}</td>
@@ -80,7 +129,7 @@
           <td>{{ item.stateId }}</td>
         </tr>
       </template>
-
+      <!-- кнопка сброса при ошибке загрузки -->
       <template v-slot:no-data>
         <v-btn color="primary" @click="init">Reset</v-btn>
       </template>
@@ -88,15 +137,14 @@
   </div>
 </template>
 
-
-
 <script>
-import moment from "moment";
+import moment from "moment"; //импорт библиотеки для работы с датами
 
 export default {
   data() {
     return {
-      dialog: false,
+      //указание базовых значений и переменных состояний
+      dialog: false, //начальное значение выключенного модального окна
       search: "",
       headers: [
         { text: "№ п/п", value: "ordNumber" },
@@ -109,10 +157,26 @@ export default {
         { text: "ИД накладной", value: "invoiceId" },
         { text: "stateId", value: "stateId" }
       ],
-      carriage: [],
-      editedIndex: -1,
-      editedItem: {},
-      defaultItem: {}
+      wagons: [], //старотовое значение массива с данными по вагонам
+      editedIndex: -1, //старотовое значение индекса редактируемого вагона в массиве
+      editedItem: {}, //старотовое значение данных по редактирумого вагону
+      validItem: 1, //состояние валидности данных по полю "время"
+
+      //создание правила для валидации поля с датой
+      rules: {
+        lastOperDtValidation: value => {
+          if (
+            moment(value, "DD.MM.YYYY HH:mm", true).isValid() ||
+            value == "-"
+          ) {
+            this.validItem = 1;
+            return true;
+          } else {
+            this.validItem = 0;
+            return 'Дата в формате "ДД.ММ.ГГГГ ЧЧ:ММ"';
+          }
+        }
+      }
     };
   },
   created() {
@@ -126,15 +190,17 @@ export default {
     }
   },
   methods: {
+    //запускаем init сразу после загрузки страницы для получения *.json и его обработки
     init() {
       fetch("table-data.json")
         .then(res => res.json())
         .then(
           res =>
-            (this.carriage = res.map(item => {
+            (this.wagons = res.map(item => {
               if (!item.lastOperDt) {
                 item.lastOperDtFormat = "-";
               } else {
+                //приводим значение времени к требуемому по ТЗ формату ДД.ММ.ГГГГ чч:мм
                 item.lastOperDtFormat = moment(item.lastOperDt).format(
                   "DD.MM.YYYY HH:mm"
                 );
@@ -144,24 +210,58 @@ export default {
         );
     },
 
+    //определяем редактируемый элемент
     editItem(item) {
-      this.editedIndex = this.carriage.indexOf(item);
+      this.editedIndex = this.wagons.indexOf(item);
       this.editedItem = Object.assign({}, item);
       this.dialog = true;
     },
 
     close() {
       this.dialog = false;
-      setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      }, 300);
     },
-
+    //перезаписываем новые значения
     save() {
-      Object.assign(this.carriage[this.editedIndex], this.editedItem);
-
-      this.close();
+      if (this.validItem == true) {
+        Object.assign(this.wagons[this.editedIndex], this.editedItem);
+        this.close();
+      } else {
+        this.close();
+      }
+    },
+    //метод сортировки значений
+    customSort(items, index, isDesc) {
+      //переопределяем значение времени в удобный к сортировке формат ISO 
+      // в переменной lastOperDtFormatNew
+      this.wagons.map(item => {
+        item.lastOperDtFormatNew = moment(
+          item.lastOperDtFormat,
+          "DD.MM.YYYY HH:mm"
+        ).format();
+      });
+      this.wagons.sort((a, b) => {
+        //сортировка по значениям времени
+        if (index == "lastOperDtFormat") {
+          if (!isDesc[0])
+            return (
+              (a.lastOperDtFormatNew < b.lastOperDtFormatNew && -1) ||
+              (b.lastOperDtFormatNew > a.lastOperDtFormatNew && 1) ||
+              (a.lastOperDtFormatNew == null && 1)
+            );
+          else
+            return (
+              (a.lastOperDtFormatNew > b.lastOperDtFormatNew && -1) ||
+              (b.lastOperDtFormatNew < a.lastOperDtFormatNew && 1) ||
+              (b.lastOperDtFormatNew == null && 1)
+            );
+        } else {
+          //сортировка по всем остальным значениям в таблице
+          if (!isDesc[0])
+            return (a[index] < b[index] && -1) || (b[index] > a[index] && 1);
+          else return (a[index] > b[index] && -1) || (b[index] < a[index] && 1);
+        }
+      });
+      return this.wagons;
     }
   }
 };
